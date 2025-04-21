3d6f850433065d36e3699c7b02f4da2760997fb3
Abd El-Rahman Amr <amr838836@gmail.com>
2025-04-21 15:06:46 +0200

    Part 05 Specifications

---
+++ Core/Domain/Specifications.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public abstract class Specifications<T>  where T : class
+    {
+        public Expression<Func<T, bool>>? Criteria { get; set; }
+        public List<Expression<Func<T, object>>> IncludeExpressions { get; set; } = new();
+        protected Specifications(Expression<Func<T, bool>>? criteria)
+        {
+            Criteria = criteria;
+        }
+        protected void AddInclude(Expression<Func<T, object>> expression) => IncludeExpressions.Add(expression);
+    }
+}
---
+++ Core/Services/Specifications/ProductWithBrandAndTypeSpecifications.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Domain.Entities;
+
+namespace Services.Specifications
+{
+    public class ProductWithBrandAndTypeSpecifications: Specifications<Product>
+    {
+        public ProductWithBrandAndTypeSpecifications(int id) : base(p=> p.Id==id)
+        {
+            AddInclude(p => p.ProductBrand);
+            AddInclude(p => p.ProductType);
+        }
+
+        public ProductWithBrandAndTypeSpecifications() : base()
+        {
+            AddInclude(p => p.ProductBrand);
+            AddInclude(p => p.ProductType);
+        }
+    }
+}
