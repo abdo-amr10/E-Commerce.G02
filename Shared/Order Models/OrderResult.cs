a3d9a6540cabf25e4f63f26a42862e3df6bb50a0
Abd El-Rahman Amr <amr838836@gmail.com>
2025-05-12 16:26:45 +0300

    Part 04 Order Service Functionality

---
+++ Shared/Order Models/OrderResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Order_Models
+{
+    public record OrderResult
+    {
+        public Guid Id { get; set; }
+        public string UserEmail { get; set; }
+        public AddressDto ShippingAddress { get; set; }
+        public ICollection<OrderItemDto> OrderItems { get; set; } = new List<OrderItemDto>();   
+        public string PaymentStatus { get; set; }
+        public string DeliveryMethod { get; set; }
+        public decimal SubTotal { get; set; }
+        public string PaymentIntentId { get; set; } = string.Empty;
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+        public decimal Total { get; set; }
+    }
+}
