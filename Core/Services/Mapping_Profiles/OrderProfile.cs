c5df2d706f88e82f5a51a043845455eed2ee13d3
Abd El-Rahman Amr <amr838836@gmail.com>
2025-05-13 00:39:32 +0300

    Part 08 Orders Controller

--- Core/Services/Mapping_Profiles/OrderProfile.cs
+++ Core/Services/Mapping_Profiles/OrderProfile.cs
@@ -18,6 +18,14 @@ namespace Services.Mapping_Profiles
                 .ForMember(d => d.ProductId, options => options.MapFrom(s => s.Product.ProductId))
                 .ForMember(d => d.ProductName, options => options.MapFrom(s => s.Product.ProductName))
                 .ForMember(d => d.PictureUrl, options => options.MapFrom(s => s.Product.PictureUrl));
+
+            CreateMap<Order, OrderResult>()
+                .ForMember(d => d.PaymentStatus, options => options.MapFrom(s => s.PaymentStatus.ToString()))
+                .ForMember(d => d.DeliveryMethod, options => options.MapFrom(s => s.DeliveryMethod.ShortName))
+                .ForMember(d => d.Total, options => options.MapFrom(s => s.SubTotal + s.DeliveryMethod.Price));
+
+
+            CreateMap<DeliveryMethod, DeliveryMethodResult>();
         }
     }
 }
