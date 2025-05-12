9c659648b61de3f1d754d3eb2a4ed7338cfd21a7
Abd El-Rahman Amr <amr838836@gmail.com>
2025-05-12 17:16:34 +0300

    Part 05 Order Profile {Mapping Profile}

---
+++ Core/Services/Mapping_Profiles/OrderProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using UserAddress = Domain.Entities.Identity.Address;
+using ShippingAddress = Domain.Entities.Order_Entities.Address;
+using Shared.Order_Models;
+using Domain.Entities.Order_Entities;
+
+namespace Services.Mapping_Profiles
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<UserAddress, AddressDto>().ReverseMap();
+
+            CreateMap<ShippingAddress, AddressDto>().ReverseMap();
+
+            CreateMap<OrderItem, OrderItemDto>()
+                .ForMember(d => d.ProductId, options => options.MapFrom(s => s.Product.ProductId))
+                .ForMember(d => d.ProductName, options => options.MapFrom(s => s.Product.ProductName))
+                .ForMember(d => d.PictureUrl, options => options.MapFrom(s => s.Product.PictureUrl));
+        }
+    }
+}
