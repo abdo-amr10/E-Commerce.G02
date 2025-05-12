7963d371ac7b24e2fc352b250b6ad1b60f50652f
Abd El-Rahman Amr <amr838836@gmail.com>
2025-05-12 14:48:24 +0300

    Part 01 Order Module Entities

---
+++ Core/Domain/Entities/Order Entities/Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Order_Entities
+{
+    public class Order : BaseEntity<Guid>
+    {
+        public Order()
+        {
+            
+        }
+        public Order(
+            string userEmail,
+            Address shippingAddress,
+            ICollection<OrderItem> orderItems,
+            DeliveryMethod deliveryMethod,
+            decimal subTotal)
+        {
+            UserEmail = userEmail;
+            ShippingAddress = shippingAddress;
+            OrderItems = orderItems;
+            DeliveryMethod = deliveryMethod;
+            SubTotal = subTotal;
+        }
+
+        public string UserEmail { get; set; }
+        public Address ShippingAddress { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+        public OrderPaymentStatus PaymentStatus { get; set; } = OrderPaymentStatus.Pending;
+        public DeliveryMethod DeliveryMethod { get; set; }
+        public int? DeliveryMethodId { get; set; }
+        public decimal SubTotal { get; set; }
+        public string PaymentIntentId { get; set; } = string.Empty;
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+
+    }
+}
---
+++ Core/Domain/Entities/Order Entities/ProductInOrderItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Order_Entities
+{
+    public class ProductInOrderItem
+    {
+        public ProductInOrderItem()
+        {
+            
+        }
+        public ProductInOrderItem(int productId, string pictureUrl, string productName)
+        {
+            ProductId = productId;
+            PictureUrl = pictureUrl;
+            ProductName = productName;
+        }
+
+        public int ProductId { get; set; }
+        public string PictureUrl { get; set; }
+        public string ProductName { get; set; }
+
+    }
+}
