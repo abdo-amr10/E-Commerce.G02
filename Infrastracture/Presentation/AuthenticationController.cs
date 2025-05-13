34d880abc4a623b091aa365b63448fe436a9fe33
Abd El-Rahman Amr <amr838836@gmail.com>
2025-05-13 15:32:24 +0300

    Part 10 Authentication Controller

--- Infrastracture/Presentation/AuthenticationController.cs
+++ Infrastracture/Presentation/AuthenticationController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Services.Abstraction;
 using Shared.Dtos;
+using Shared.Order_Models;
 
 namespace Presentation
 {
@@ -19,6 +22,35 @@ namespace Presentation
         public async Task<ActionResult<UserResultDto>> Register(RegisterDto registerDto)
         => Ok(await serviceManger.AuthenticationService.RegisterAsync(registerDto));
 
+        [HttpGet("EmailExists")]
+        public async Task<ActionResult<bool>> CheckEmailExist(string email)
+            => Ok(await serviceManger.AuthenticationService.CheckEmailExist(email));
 
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<UserResultDto>> GetCurrentUser()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var result = await serviceManger.AuthenticationService.GetUserByEmail(email);
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpGet("Address")]
+        public async Task<ActionResult<AddressDto>> GetAddress()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var result = await serviceManger.AuthenticationService.GetUserAddress(email);
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpPut("Address")]
+        public async Task<ActionResult<AddressDto>> UpdateAddress(AddressDto address)
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var result = await serviceManger.AuthenticationService.UpdateUserAddress(address, email);
+            return Ok(result);
+        }
     }
 }
